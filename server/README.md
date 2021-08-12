3cab71930179b32fd391e0b039af170217c4eaf2
Luu Nguyen <nxuluu@gmail.com>
2021-08-12 23:08:58 +0700

    Setup mongoDb

--- server/README.md
+++ server/README.md
@@ -2,4 +2,5 @@
 npm init
 npm install --save-dev nodemon: For dev
 npm install express --save: expressjs
-
+npm install mongoose --save
+npm install body-parser - dùng cho req.body dùng để truyền dữ liệu post
---
+++ server/controllers/user.controller.js
@@ -0,0 +1,26 @@
+﻿import User from '../models/user.model.js';
+
+export async function getUser(req, res) {
+    try {
+        const users = await User.find();
+        console.log(users);
+        res.json(users);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export async function postUser(req, res) {
+    try {
+        console.log(req.body);
+        const user = new User({
+            name: req.body.name,
+            password: req.body.password
+        });
+
+        user.save();
+        res.redirect('/');
+    } catch (err) {
+        console.log(err);
+    }
+}
